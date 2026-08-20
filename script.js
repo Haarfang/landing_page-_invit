fac4c56aeaa5561103d6fd4eb78dd044924f7ca8
Haarfang <loicp.didat@gmail.com>
2026-08-20 18:30:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,6 @@ const questions = [
         answers: [
             "🍖 Menu classique",
             "🌿 Menu végétarien",
-            "🥕 Menu végétalien",
             "🧒 Menu enfant",
             "📜 Autre / besoin particulier"
         ]
@@ -109,6 +108,8 @@ const questions = [
 let currentQuestion = 0;
 
 let answers = [];
+
+let declined = false;
 
 
 /* =========================================
